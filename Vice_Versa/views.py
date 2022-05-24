9bff3c239db4f9de7a1741f5c97d32181123ab37
olehmatskiv <85799629+olehmatskiv@users.noreply.github.com>
2022-05-24 16:13:57 +0300

    create templates

--- Vice_Versa/views.py
+++ Vice_Versa/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def about(request):
     return HttpResponse('This is main page')
 
 def home(request):
-    return HttpResponse('HOME PAGE')
+    return render(request, 'index.html' )
