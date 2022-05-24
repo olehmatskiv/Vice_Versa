c15757058e4e113c8203c45dad0b71ece934d2ae
olehmatskiv <85799629+olehmatskiv@users.noreply.github.com>
2022-05-25 00:15:15 +0300

    Finished project

--- Vice_Versa/views.py
+++ Vice_Versa/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def about(request):
-    return HttpResponse('This is main page')
+
 
 def home(request):
     return render(request, 'index.html' )
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request , 'reverse.html' ,{'usertext' : user_text , 'reversedtext' : reversed_text }) 
